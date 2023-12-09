4ae630bf60fc3e71e6d093f13647b3485002a81a
Mariem <mariem.ehab@ieee.org>
2023-12-09 07:14:44 -0800

    testing City

--- tests/test_models/test_city.py
+++ tests/test_models/test_city.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 """test city class"""
 import unittest
-
+from models.base_model import BaseModel
+from models.city import City
 
 class TestState(unittest.TestCase):
     """test state class"""
     def test_city(self):
         """test of city"""
-        pass
+        obj = City()
+        self.assertEqual(isinstance(obj, BaseModel), True)
+        self.assertEqual(isinstance(obj, City), True)
+        self.assertEqual(type(obj.name), str)
+        self.assertEqual(type(obj.state_id), str)
 
 if __name__ == '__main__':
     unittest.main()

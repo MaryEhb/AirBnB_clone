59ebb6fc19696a1788308bdf911db17bbefa6cd1
Mariem <mariem.ehab@ieee.org>
2023-12-09 09:25:57 -0800

    empty test for checker

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#/usr/bin/python3
+"""test state class"""
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+
+class TestEngine(unittest.TestCase):
+    """test state class"""
+    def test_storage(self):
+        """test of state"""
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

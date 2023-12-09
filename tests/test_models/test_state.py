6d99e4fd26a5012b90040d6649c1e3212bf3ba64
Mariem <mariem.ehab@ieee.org>
2023-12-09 07:10:14 -0800

    state test

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,13 +1,19 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 """test state class"""
 import unittest
+from models.base_model import BaseModel
+from moodels.state import State
 
 
 class TestState(unittest.TestCase):
     """test state class"""
     def test_state(self):
         """test of state"""
-        pass
+        obj = State()
+        self.assertEqual(isinstance(obj, BaseModel), True)
+        self.assertEqual(isinstance(obj, State), True)
+        self.assertEqual(obj.__class__.__name__, 'State')
+        self.assertEqual(type(obj.name), str)
 
 if __name__ == '__main__':
     unittest.main()

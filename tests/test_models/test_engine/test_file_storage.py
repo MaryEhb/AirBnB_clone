46dfa92b011965292c20c6f34e5e8d3ac17e9b1c
Mariem <mariem.ehab@ieee.org>
2023-12-10 02:31:29 -0800

    adding half of storage tests

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -3,12 +3,53 @@
 import unittest
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
-
+import os
+import json
+from models import storage
 
 class TestEngine(unittest.TestCase):
-    """test state class"""
+    """test file storage class"""
+
+    def setUp(self):
+        try:
+            os.rename("file.json", "tmp")
+        except IOError:
+            pass
+
+    def tearDown(self):
+        try:
+            os.remove("file.json")
+        except IOError:
+            pass
+        try:
+            os.rename("tmp", "file.json")
+        except IOError:
+            pass
+
+        FileStorage._FileStorage__objects = {}
+
     def test_storage(self):
-        """test of state"""
+        self.assertEqual(type(storage), FileStorage)
+
+    def test_no_args(self):
+        self.assertEqual(type(FileStorage()), FileStorage)
+
+    def test_path_type_str(self):
+        self.assertEqual(type(FileStorage._FileStorage__file_path), str)
+
+    def test_obj_type_dict(self):
+         self.assertEqual(type(FileStorage._FileStorage__objects), dict)
+
+    def test_all(self):
+        pass
+
+    def test_new(self):
+        pass
+
+    def test_save(self):
+        pass
+
+    def test_reload(self):
         pass
 
 if __name__ == '__main__':
